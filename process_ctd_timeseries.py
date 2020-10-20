3412b05df46241a3ae1f86fd1e398aaf527d822b
Jessy Barrette <jessy.barrette@hakai.org>
2020-10-19 17:18:51 -0700

    Add step to read Seabird CNV and output NetCDF

--- process_ctd_timeseries.py
+++ process_ctd_timeseries.py
@@ -2,7 +2,8 @@ import google
 import hakai
 import process
 
-import seabird
+from seabird.cnv import fCNV
+from seabird.netcdf import cnv2nc
 import xarray as xr
 import pandas as pd
 import numpy as np
@@ -10,7 +11,7 @@ import glob
 
 # File path
 #dest_dir = r"/mnt/d/hakai_CTD/"
-dest_dir = r"D:\hakai_CTD/"
+dest_dir = r"D:/hakai_CTD/"
 
 # Define Spreadsheet ID
 # Hakai ADCP Deployment log
@@ -27,14 +28,26 @@ df = google.convert_google_sheet_to_dataframe(val)
 #  - Retrieve instrument time offset from UTC
 #  - Convert lat/long to decimal degrees
 #  - Compute trilateration if available
-#  - Retrieve Magnetic Declination
 #  - Generate standard Hakai File Name
 df = hakai.transform_hakai_log(df, dest_dir)
 
 # Download CTD data
 # Create a dictionary with the output file name as key and link to the data as value
-adcp_files = {row['file_name']+'.cnv': row['Link to Raw Data'] for index, row in df.iterrows()}
-hakai.download_file(adcp_files, dest_dir)
+ctd_files = {row['file_name']+'.cnv': row['Link to Raw Data'] for index, row in df.iterrows()}
+#hakai.download_file(ctd_files, dest_dir)
 
+# Loop through each files
+for index, row in df.iterrows():
+    if row['Link to Raw Data']:
+        # Read Seabird CNV
+        print('Read '+row['file_name']+'.cnv')
+        c = fCNV(dest_dir + row['file_name']+'.cnv')
+        # Run QARTOD
+
+        # Add Metadata
+
+        # Save to NetCDF
+        print('Save to '+row['file_name']+'.nc')
+        cnv2nc(c, dest_dir + row['file_name']+'.nc')
 
 print('works!')
