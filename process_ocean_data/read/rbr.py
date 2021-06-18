310c5e82e9387135d38643c5fcfd22c4d344242c
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-18 16:47:42 -0700

    initial commit or rbr legacy rtext reader

---
+++ process_ocean_data/read/rbr.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import re
+
+
+def rtext(file_path):
+    """
+    Read RBR R-Text format.
+    :param file_path: path to file to read
+    :return: metadata dictionary dataframe
+    """
+    # MON File Header end
+    header_end = "NumberOfSamples"
+
+    with open(file_path) as fid:
+        line = ""
+        section = "header_info"
+        metadata = {section: {}}
+
+        while not line.startswith(header_end):
+            # Read line by line
+            line = fid.readline()
+
+            if re.match(r"\s*.*(=).*", line):
+                key, item = re.split(r"\s*[:=]\s*", line, 1)
+
+                if re.match(r'.*\[\d+\]\..*', key):
+                    items = re.search(r'(.*)\[(\d+)\]\.(.*)', key)
+                    key = items[1]
+                    index = items[2]
+                    subkey = items[3].strip()
+
+                    if key not in metadata:
+                        metadata[key] = {}
+                    if index not in metadata[key]:
+                        metadata[key][index] = {}
+
+                    metadata[key][index][subkey] = item.strip()
+
+                else:
+                    metadata[key] = item.strip()
+            else:
+                print(f"Ignored: {line}")
+        # Read NumberOFSamples line
+        metadata['NumberOfSamples'] = int(line.rsplit('=')[1])
+
+        # Read data
+        df = pd.read_csv(fid, sep='\s\s+')
+
+        # Make sure that line count is good
+        if len(df) != metadata['NumberOfSamples']:
+            raise RuntimeError('Data length do not match expected Number of Samples')
+
+        return df, metadata
