894571acad26a483c28cf419d298a2920ba11c44
Jessy Barrette <jessy.barrette@hakai.org>
2022-05-26 17:51:49 -0700

    fix bottle parser

--- process_ocean_data/read/seabird/__main__.py
+++ process_ocean_data/read/seabird/__main__.py
@@ -6,8 +6,12 @@ import xmltodict
 import json
 import os
 
+import argparse
+
+SBE_TIME_FORMAT = '%B %d %Y %H:%m:%s' # Jun 23 2016 13:51:30
 logger = logging.getLogger(__name__)
 # TODO load reference json file
+
 reference_vocabulary_path = os.path.join(os.path.dirname(__file__),'seabird_variable_attributes.json')
 with open(reference_vocabulary_path) as f:
     seabird_variable_attributes = json.load(f)
@@ -19,6 +23,8 @@ def add_seabird_vocabulary(variable_attributes):
     for var in variable_attributes.keys():
         if var in seabird_variable_attributes:
             variable_attributes[var].update(seabird_variable_attributes[var])
+        else:
+            logger.warning(f'Unknown variable {var}')
     return variable_attributes
 
 def cnv(file_path, output='xarray'):
@@ -36,16 +42,47 @@ def cnv(file_path, output='xarray'):
 def btl(file_path,output='xarray'):
     with open(file_path) as f:
         header = parse_seabird_file_header(f)
-        header['variables'] = add_seabird_vocabulary(header['variables'])
-        # TODO parse column header with fix width
-        # TODO parse data with fixed width method and separate statistical data
-        df = pd.read_fwf(f,widths=[12,14]+ [10]*int(header['nquan']), names=header['variables'].keys())
-    # TODO Split statistical data info separate dateframes
+        if header['variables']:
+            header['variables'] = add_seabird_vocabulary(header['variables'])
+        else:
+            # Retrieve variables from bottle header and lowwer the first letter of each variable
+            header['variables'] = add_seabird_vocabulary({var[0].lower()+var[1:]:{} for var in header['bottle_columns']})
+        # parse column header with fix width
+        variable_list = list(header['variables'].keys())
+        variable_list += ['stats']
+        df = pd.read_fwf(f,widths=[10,12]+ [11]*(len(header['bottle_columns'])-1), names=variable_list)
 
-    # TODO Regroup dataframes by mathing nearest index prior
+    # Split statistical data info separate dateframes
+    df_avg = df.iloc[::2,:].reset_index()
+    df_std = df.iloc[1::2,:].reset_index().add_suffix('_sdev')
+    df = df_avg.join(df_std)
+
+    # Generate time variable
+    df['time'] = pd.to_datetime(df['date'] + ' ' + df['date_sdev'])
+    
+    # Ignore extra variables
+    df = df.drop(columns=['date','date_sdev','stats','stats_sdev','index_sdev','bottle_sdev'])
+
+    # Improve metadata
+    n_scan_per_bottle =  int(header['datcnv_scans_per_bottle'])
+    header = generate_seabird_cf_history(header)
     if output == 'dataframe':
         return df, header
-    return convert_sbe_dataframe_to_dataset(df,header)
+
+    # Convert to xarray
+    ds = convert_sbe_dataframe_to_dataset(df,header)
+
+    # Add attributes to std variables and add cell_method
+    for var in ds:
+        var_std = var + "_sdev"
+        if var_std in ds:
+            ds[var_std].attrs = ds[var].attrs
+            ds[var_std].attrs['cell_method'] = f"scan: standard_deviation (previous {n_scan_per_bottle} scans)"
+            # TODO confirm that seabird uses the previous records from this timestamp
+        if var not in ['time','bottle']:
+            ds[var].attrs['cell_method'] = f"scan: mean (previous {n_scan_per_bottle} scans)"
+
+    return ds 
     
 
 def convert_sbe_dataframe_to_dataset(df,header):
@@ -55,13 +92,16 @@ def convert_sbe_dataframe_to_dataset(df,header):
     ds = df.to_xarray()
     variable_attributes = header.pop('variables')
     for var,attrs in variable_attributes.items():
+        if var not in ds: 
+            continue
         ds[var].attrs = attrs 
     ds.attrs = header
     return ds
 
 
 def parse_seabird_file_header(f):
-    def unknown_line():
+    def unknown_line(line):
+        header['comments'] += [line]
         logger.warning(f'Unknown line format: {line}')
     def standardize_attribute(attribute):
         return attribute.strip().replace(' ','_').lower()
@@ -76,12 +116,13 @@ def parse_seabird_file_header(f):
         if " = " in line:
              attr, value = line[2:].split('=',1)
              header[standardize_attribute(attr)] = value.strip()
-        elif line.startswith('* Sea-Bird'):
-            header['instrument_type'] += " " + re.search('\* Sea-Bird (.*) Data File\:',line)[1].strip()
-        elif "Software version" in line:
-            header['software_version'] = re.search('\* Software version (.*)',line)[1]
+        elif line.startswith(('* Sea-Bird', '* SBE ')) :
+            instrument_type = re.search('\* Sea-Bird (.*) Data File\:|\* SBE (.*)',line).groups()
+            header['instrument_type'] += "".join([item for item in instrument_type if item])
+        elif re.match(r"\* Software version .*",line, re.IGNORECASE):
+            header['software_version'] = re.search('\* Software version (.*)',line,re.IGNORECASE)[1]
         else:
-            unknown_line()
+            unknown_line(line)
 
     def read_number_line(line):
         if line.startswith('# name'):
@@ -98,14 +139,14 @@ def parse_seabird_file_header(f):
             attr, value = line[2:].split('=',1)
             header[standardize_attribute(attr)] = value.strip()
         else:
-            unknown_line()
-    line = ""
+            unknown_line(line)
+    line = "*"
     header = {}
     header['variables'] = {}
     header['instrument_type'] = ""
     header['comments'] = []
     read_next_line = True
-    while "*END*"  not in line:
+    while "*END*"  not in line and line.startswith(('*','#')):
 
         if read_next_line:
             line = f.readline()
@@ -113,7 +154,7 @@ def parse_seabird_file_header(f):
             read_next_line = True
 
         # Ignore empty lines or last header line
-        if re.match('^\*\s*$',line) or '*END*' in line:
+        if re.match('^\*\s*$',line) or '*END*' in line or re.match('^\s*Bottle .*',line):
             continue
 
         if re.match('(\*|\#)\s*\<',line):
@@ -146,14 +187,24 @@ def parse_seabird_file_header(f):
         elif line.startswith('# '):
             read_number_line(line)
         else:
-            unknown_line()
+            unknown_line(line)
     # Remap variables to seabird variables
     variables = {attrs['sbe_variable']: attrs for key,attrs in header['variables'].items()}
     header['variables'] = variables
+
+    # btl header row
+    if 'Bottle' in line:
+        var_columns = line[22:-1]
+        var_width = 11
+        header['bottle_columns'] = ['Bottle','Date'] + [var_columns[index:index+var_width].strip() for index in range(0, len(var_columns),var_width)]
+        # Read  Position Time line
+        line = f.readline()
+    
+    header['comments'] = '\n'.join(header['comments'])
     return header
             
 def generate_seabird_cf_history(attrs, drop_processing_attrs=False):
-    sbe_processing_steps = ('datcnv',)
+    sbe_processing_steps = ('datcnv','bottlesum')
     history = []
     for step in sbe_processing_steps:
         step_attrs = {key.replace(step+'_',''): value for key,value in attrs.items()if key.startswith(step)}
@@ -171,10 +222,23 @@ def generate_seabird_cf_history(attrs, drop_processing_attrs=False):
     if drop_processing_attrs:
         drop_keys = [key for key in attrs.keys() if key.startswith(sbe_processing_steps)]
         for key in drop_keys:
-            attrs.pop(key)
-    
+            attrs.pop(key)    
     return attrs
     
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Parse Seabird Instruments File Formats')
+    parser.add_argument('--input',help='file inptut (btl or cnv')
+
+    args = parser.parse_args()
+    if args.input:
+        if args.input.endswith('btl'):
+            btl(args.input)
+        elif args.input.endswith('cnv'):
+            cnv(args.input)
+
+
+
 # TODO add console input
 test_file_path = "/Users/jessybarrette/repo/process_ocean_timeseries/process_ocean_data/read/test/seabird/MI18MHDR.btl"
 output = btl(test_file_path)
